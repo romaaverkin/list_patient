b2681d8b80c8ebf26d6cb048623151fed6d702e1
Roman <romaaverkin@gmail.com>
2021-06-24 12:21:26 +0700

    First commit

---
+++ src/main/java/romaaverkin/gmail/com/list_patient/ListPatientApplication.java
@@ -0,0 +1,13 @@
+package romaaverkin.gmail.com.list_patient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListPatientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListPatientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/romaaverkin/gmail/com/list_patient/ListPatientApplicationTests.java
@@ -0,0 +1,13 @@
+package romaaverkin.gmail.com.list_patient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListPatientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
